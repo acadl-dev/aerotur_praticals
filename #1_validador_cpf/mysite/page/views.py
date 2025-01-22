a1557e3050ff57f4e43d345e33b0d7c9f6a2441f
acadl-dev <acadl.dev@outlook.com>
2025-01-22 12:06:56 -0300

    Quinto commit: tudo funcionando. Futuramente adicionar uma validação para o cpf e eliminar o botão de submit

--- #1_validador_cpf/mysite/page/views.py
+++ #1_validador_cpf/mysite/page/views.py
@@ -3,15 +3,19 @@ from .utils import *
 from django.contrib import messages
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def home(request):
     return render(request, 'home.html')
 
+@csrf_exempt
 def validador_cpf(request, cpf):
     valido = valida_cpf(cpf)  # Chama a função de validação
     if valido:
-        return JsonResponse({'valido': True, 'mensagem': f"CPF valido!"}, status=200)
-    return JsonResponse({'valido': False, 'mensagem': "CPF invalido!"}, status=200)
+        response_data = {'valido': True, 'mensagem': f"CPF valido!"}
+        return JsonResponse(response_data, status=200)
+    response_data = {'valido': False, 'mensagem': "CPF invalido!"}
+    return JsonResponse(response_data, status=200)
 
 
 
