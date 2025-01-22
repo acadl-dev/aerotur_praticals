4282b28b34d9d923730608e2b05bbc027b7a79f0
acadl-dev <acadl.dev@outlook.com>
2025-01-22 14:05:33 -0300

    Adicionado o segundo projeto: cosumir uma API de CEP

---
+++ #2_consumir_api_cep/api_cep/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './cep/templates/**/*.html',  // Ajuste para onde estão seus arquivos HTML
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
