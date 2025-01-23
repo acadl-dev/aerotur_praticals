5bc8951647adec0960232de6fd2ea8e2bd4b0f5d
acadl-dev <acadl.dev@outlook.com>
2025-01-23 10:34:06 -0300

    Finalizado o segundo projeto para consumir uma API de CEP. Pendente de estilização e 'validar' a entrada do CEP

--- #2_consumir_api_cep/api_cep/cep/urls.py
+++ #2_consumir_api_cep/api_cep/cep/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('executarAPICEP/<str:cep>/', views.executarAPICEP, name='executar_api_cep'),
     
 ]
+
--- #2_consumir_api_cep/api_cep/cep/views.py
+++ #2_consumir_api_cep/api_cep/cep/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+import requests
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+@csrf_exempt
+def executarAPICEP(request, cep):
+    url = f"https://viacep.com.br/ws/{cep}/json/"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return JsonResponse(data)
+    else:
+        return JsonResponse({'error': 'Unable to fetch data from API'}, status=response.status_code)
+    
