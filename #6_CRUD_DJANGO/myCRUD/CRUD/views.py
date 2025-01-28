1a4e1432567ad0f7d4482228e6bfcbe7d9ec0258
acadl-dev <acadl.dev@outlook.com>
2025-01-28 13:43:50 -0300

    Adição do projeto de blog do livro DJANGO by example para praticar um CRUD

---
+++ #6_CRUD_DJANGO/myCRUD/CRUD/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Post
+from django.http import Http404
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(
+        request,
+        'CRUD/post/list.html',
+        {'posts': posts}
+    )
+    
+def post_detail(request, id):
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status=Post.Status.PUBLISHED
+    )
+    return render(
+        request,
+        'CRUD/post/detail.html',
+        {'post': post}
+    )
